ca07c6fe7f24bb2f952f9a6d4c7eeeaa25f921d3
Brandon <leebran@onid.oregonstate.edu>
2016-04-20 01:48:31 -0700

    Initial Commit

---
+++ CameraTest/CameraTest/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  CameraTest
+//
+//  Created by Brandon Lee on 4/20/16.
+//  Copyright © 2016 Brandon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func cameraButton(sender: AnyObject) {
+
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .Camera
+            
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
+
+    @IBAction func cameraRollButton(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .PhotoLibrary
+        
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
